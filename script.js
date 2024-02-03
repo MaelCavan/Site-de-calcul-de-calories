28743b6a78fef6c7aaab232b548447824dfd1a42
MaelCavan <78815390+MaelCavan@users.noreply.github.com>
2024-02-03 08:59:51 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function calculate() {
-    // Récupérer les valeurs du poids et de la taille
+        document.getElementById("resultimc").innerHTML = "Votre IMC est : <span id='imcValue'></span>";
+        document.getElementById("resultnap").innerHTML = "Votre NAP (niveau d'activité physique) est : <span id='napValue'></span>";
+
     var year = document.getElementById("year").value;
     var weight = document.getElementById("weight").value;
     var height = document.getElementById("height").value;
@@ -32,21 +34,69 @@ function calculate() {
     var cal=NAP*MB
 
     // Afficher
-    document.getElementById("imcValue").innerText = imc.toFixed(2);
-    document.getElementById("mbValue").innerText = MB.toFixed(2);
+    document.getElementById("imcValue").innerText = imc.toFixed(1);
+    document.getElementById("mbValue").innerText = MB.toFixed(0);
     document.getElementById("napValue").innerText = NAP.toFixed(2);
-    document.getElementById("calValue").innerText = cal.toFixed(2);
+    document.getElementById("calValue").innerText = cal.toFixed(0);
 
     // Afficher le résultat
      var resultElementimc = document.getElementById("resultimc");
-     resultElementimc.style.display = "block";
-
     var resultElementmb = document.getElementById("resultmb");
-    resultElementmb.style.display = "block";
-
     var resultElementnap = document.getElementById("resultnap");
+    var resultElementcal = document.getElementById("resultcal");
+
+     resultElementimc.style.display = "block";
+                if (imc < 18.5) {
+                    resultElementimc.innerHTML += "<br> Vous êtes en maigreur";
+                } else if (NAP < 1.59) {
+			if (imc > 30) {
+                    	resultElementimc.innerHTML += "<br> Vous êtes en obesite";
+			}  else if (imc > 25) {
+                    resultElementimc.innerHTML += "<br> Vous êtes en surpoids";
+                }}
+
+    resultElementmb.style.display = "block";
+                if (NAP < 1.39) {
+                    resultElementnap.innerHTML += "<br> Vous êtes sédentaire";
+                } else if (NAP < 1.59) {
+                    resultElementnap.innerHTML += "<br> Vous êtes peu actif";
+                } else if (NAP < 1.89) {
+                    resultElementnap.innerHTML += "<br> Vous êtes actif";
+                } else {
+                    resultElementnap.innerHTML += "<br> Vous êtes très actif";
+                }
     resultElementnap.style.display = "block";
 
-    var resultElementcal = document.getElementById("resultcal");
     resultElementcal.style.display = "block";
+     if (NAP<1.59) {
+    protmin=0.8*weight
+    protmax=1.2*weight
+    } else {
+    protmin=1.2*weight
+    protmax=1.6*weight
+    }
+    document.getElementById("protValuemin").innerText = protmin.toFixed(0);
+    var resultElementprotmin = document.getElementById("resultprot");
+    resultElementprotmin.style.display = "block";
+    document.getElementById("protValuemax").innerText = protmax.toFixed(0);
+    var resultElementprotmax = document.getElementById("resultprot");
+    resultElementprotmax.style.display = "block";
+
+    glucidemin=0.5*cal/4
+    glucidemax=0.6*cal/4
+    document.getElementById("glucideValuemax").innerText = glucidemax.toFixed(0);
+    var resultElementglucidemax = document.getElementById("resultglucide");
+    resultElementglucidemax.style.display = "block";
+    document.getElementById("glucideValuemin").innerText = glucidemin.toFixed(0);
+    var resultElementglucidemin = document.getElementById("resultglucide");
+    resultElementglucidemin.style.display = "block";
+
+    lipidemin=0.4*cal/9
+    lipidemax=0.5*cal/9
+    document.getElementById("lipideValuemax").innerText = lipidemax.toFixed(0);
+    var resultElementlipidemax = document.getElementById("resultlipide");
+    resultElementlipidemax.style.display = "block";
+    document.getElementById("lipideValuemin").innerText = lipidemin.toFixed(0);
+    var resultElementlipidemin = document.getElementById("resultlipide");
+    resultElementlipidemin.style.display = "block";
 }
